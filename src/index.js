b306c16446a6f094e0dc9c2e6f737c14ee22eba9
Durgasri <challadurgasri2000@gmail.com>
2025-05-23 00:27:40 +0530

    Included AuthContextProvider and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AuthContextProvider from "./context/Auth/AuthState";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* Include your context provider and browser Router here */}
+    <AuthContextProvider>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
